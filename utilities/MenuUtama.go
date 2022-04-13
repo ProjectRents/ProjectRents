188ea8033dad7e14363b5f6389a300a893b09b7c
alkaprasetya <66933824+alkaprasetya@users.noreply.github.com>
2022-04-13 14:23:11 +0700

    tambah fitur ubah buku

--- utilities/MenuUtama.go
+++ utilities/MenuUtama.go
@@ -12,7 +12,7 @@ func MenuUtama() {
 	// Get database connection
 	connection := config.ReadEnv()
 	db := config.Database(connection)
-	
+
 	// Migrate tables
 	// db.AutoMigrate(&entities.User{})
 	// db.AutoMigrate(&entities.Book{})
@@ -33,88 +33,92 @@ func MenuUtama() {
 		}
 
 		switch line {
-			case "1":
-				// Add Account
-				Input := datastore.UserDB{DB: db}
+		case "1":
+			// Add Account
+			Input := datastore.UserDB{DB: db}
 
-				fmt.Println("MENAMBAHKAN AKUN BARU")
-				separator()
-				fmt.Println(Input.CreateUser(InputUser()))
-				separator()
-			case "2":
-				// Login
-				Input := datastore.UserDB{DB: db}
+			fmt.Println("MENAMBAHKAN AKUN BARU")
+			separator()
+			fmt.Println(Input.CreateUser(InputUser()))
+			separator()
+		case "2":
+			// Login
+			Input := datastore.UserDB{DB: db}
 
-				fmt.Println("MENU LOGIN")
-				separator()
-				result, kondisi := Input.Login(InputLogin())
+			fmt.Println("MENU LOGIN")
+			separator()
+			result, kondisi := Input.Login(InputLogin())
 
-				if kondisi == true {
-					fmt.Println("Selamat datang,", result)
-				} else {
-					fmt.Println(result)
+			if kondisi == true {
+				fmt.Println("Selamat datang,", result)
+			} else {
+				fmt.Println(result)
+				break
+			}
+			separator()
+
+			listLogin()
+			Login := bufio.NewScanner(os.Stdin)
+			for Login.Scan() {
+				line := Login.Text()
+				if line == "99" {
+					fmt.Println("Logout...")
+					separator()
 					break
 				}
-				separator()
-				
-				listLogin()
-				Login := bufio.NewScanner(os.Stdin)
-				for Login.Scan(){
-					line := Login.Text()
-					if line == "99" {
-						fmt.Println("Logout...")
-						separator()
-						break
-					}
 
-					switch line {
-						case "1":
-							fmt.Println("Lihat Profil")
-							separator()
-						case "2":
-							fmt.Println("Daftar Buku Saya")
-							separator()
-						case "3":
-							Input := datastore.BookDB{DB: db}
+				switch line {
+				case "1":
+					fmt.Println("Lihat Profil")
+					separator()
+				case "2":
+					fmt.Println("Daftar Buku Saya")
+					separator()
+				case "3":
+					Input := datastore.BookDB{DB: db}
 
-							fmt.Println("MENAMBAHKAN BUKU BARU")
-							separator()
-							fmt.Println(Input.CreateBook(InputBook()))
-							separator()
-						case "4":
-							fmt.Println("Update Buku")
-							separator()
-						case "5":
-							fmt.Println("Hapus Buku")
-							separator()
-						case "6":
-							fmt.Println("Pinjam Buku")
-							separator()
-						case "7":
-							fmt.Println("Kembalikan Buku")
-							separator()
-						case "99":
-							fmt.Println("Logout...")
-						default:
-							fmt.Println("Menu Tidak Tersedia")
-					}
+					fmt.Println("MENAMBAHKAN BUKU BARU")
+					separator()
+					fmt.Println(Input.CreateBook(InputBook()))
+					separator()
+				case "4":
+					Input := datastore.BookDB{DB: db}
 
-					listLogin()
+					fmt.Println("MENGUBAH BUKU")
+					separator()
+					fmt.Println(Input.UpdateBook(ubahBook()))
+					separator()
+				case "5":
+					fmt.Println("Hapus Buku")
+					separator()
+				case "6":
+					fmt.Println("Pinjam Buku")
+					separator()
+				case "7":
+					fmt.Println("Kembalikan Buku")
+					separator()
+				case "99":
+					fmt.Println("Logout...")
+				default:
+					fmt.Println("Menu Tidak Tersedia")
 				}
-			case "3":
-				fmt.Println("Lihat Daftar Buku")
-				separator()
-			case "99":
-				fmt.Println("Exit")
-			default:
-				fmt.Println("Menu Tidak Tersedia")
-				separator()
+
+				listLogin()
+			}
+		case "3":
+			fmt.Println("Lihat Daftar Buku")
+			separator()
+		case "99":
+			fmt.Println("Exit")
+		default:
+			fmt.Println("Menu Tidak Tersedia")
+			separator()
 		}
 
 		list()
 	}
-		
+
 	if err := Menu.Err(); err != nil {
 		fmt.Println("Error encountered:", err)
-	}	
+	}
 }
---
+++ utilities/ubahbook.go
@@ -0,0 +1,26 @@
+package utilities
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ubahBook() (title, isbn, author string) {
+	fmt.Print("Masukkan judul buku: ")
+	Title := bufio.NewScanner(os.Stdin)
+	Title.Scan()
+	title = Title.Text()
+
+	fmt.Print("Masukkan kode ISBN: ")
+	Isbn := bufio.NewScanner(os.Stdin)
+	Isbn.Scan()
+	isbn = Isbn.Text()
+
+	fmt.Print("Masukkan nama pengarang: ")
+	Author := bufio.NewScanner(os.Stdin)
+	Author.Scan()
+	author = Author.Text()
+
+	return
+}
