fc2e814cc9c5cb81b3a9de8115a6039d8627e91f
alkaprasetya <66933824+alkaprasetya@users.noreply.github.com>
2022-04-14 11:51:16 +0700

    update database User

--- entities/User.go
+++ entities/User.go
@@ -4,9 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name string `gorm:"type:varchar(35);not null"`
-	Email string `gorm:"type:varchar(50);not null;unique"`
+	Name     string `gorm:"type:varchar(35);not null"`
+	Email    string `gorm:"type:varchar(50);not null;unique"`
 	Password string `gorm:"type:varchar(50);not null"`
-	Alamat string `gorm:"type:varchar(35);not null"`
-	Books []Book
+	Alamat   string `gorm:"type:varchar(35);not null"`
+	Books    []Book
+	Rents    []Rent
 }
